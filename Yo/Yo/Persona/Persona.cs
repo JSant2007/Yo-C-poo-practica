d00f716620a5f40bf743f2b348d7b547b7434231
JSant <85200051+JSant2007@users.noreply.github.com>
2022-02-05 18:31:44 -0500

    Archivos Subidos
    1

---
+++ Yo/Yo/Persona/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Yo.Persona
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public char genero;
+        
+        public char Genero
+        {
+            get { return Convert.ToChar(genero); }
+            set { genero = value; }
+        }
+            
+        public string personalidad { get; set; }
+        
+        public Persona(string nombre, int año)
+        {
+            this.nombre = nombre;
+            this.edad = año;
+        }
+        
+        public override string ToString()
+        {
+            return $"Nombre: {nombre}, edad: {edad} ";
+        }
+
+    }
+}
---
+++ Yo/Yo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Yo.Persona;
+
+namespace Yo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Crear Persona");
+            Console.WriteLine("Inserta Nombre");
+            string nombre1 = Console.ReadLine();
+            Console.WriteLine("Inserta Edad");
+            int edad2 = Convert.ToInt32(Console.ReadLine());
+
+            var Yo = new Persona.Persona(nombre1, edad2);
+
+            Console.WriteLine(Yo);
+        }
+    }
+}
